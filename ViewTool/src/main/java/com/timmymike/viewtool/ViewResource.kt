75c7399725e5bce025f26b1932bf2e3b27797384
timmyhsieh0916 <timmy@everise.digital>
2025-07-15 11:11:16 +0800

    1.新增getResourceStringArray系列方法

--- ViewTool/src/main/java/com/timmymike/viewtool/ViewResource.kt
+++ ViewTool/src/main/java/com/timmymike/viewtool/ViewResource.kt
@@ -3,11 +3,10 @@ package com.timmymike.viewtool
 import android.content.Context
 import android.content.res.Resources
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.util.DisplayMetrics
 import android.view.View
 import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.Fragment
 
 
@@ -19,15 +18,19 @@ import androidx.fragment.app.Fragment
  * */
 fun Context.getResourceColor(colorId: Int) = kotlin.runCatching { ContextCompat.getColor(this, colorId) }.getOrNull() ?: Color.TRANSPARENT
 
-fun Context.getResourceDrawable(drawableID: Int) = kotlin.runCatching { ContextCompat.getDrawable(this, drawableID) }.getOrNull() ?: ColorDrawable(Color.TRANSPARENT)
+fun Context.getResourceDrawable(drawableID: Int) = kotlin.runCatching { ContextCompat.getDrawable(this, drawableID) }.getOrNull() ?: Color.TRANSPARENT.toDrawable()
 
 fun Context.getResourceString(stringID: Int) = kotlin.runCatching { this.resources.getString(stringID) }.getOrNull() ?: ""
+
+fun Context.getResourceStringArray(arrayId: Int): Array<String> = kotlin.runCatching { this.resources.getStringArray(arrayId) }.getOrNull() ?: arrayOf()
 
 fun Context.getResourceColorOrNull(colorId: Int) = kotlin.runCatching { ContextCompat.getColor(this, colorId) }.getOrNull()
 
 fun Context.getResourceDrawableOrNull(drawableID: Int) = kotlin.runCatching { ContextCompat.getDrawable(this, drawableID) }.getOrNull()
 
 fun Context.getResourceStringOrNull(stringID: Int) = kotlin.runCatching { this.resources.getString(stringID) }.getOrNull()
+
+fun Context.getResourceStringArrayOrNull(arrayId: Int): Array<String>? = kotlin.runCatching { this.resources.getStringArray(arrayId) }.getOrNull()
 
 
 fun View.getResourceColor(colorId: Int) = this.context.getResourceColor(colorId)
@@ -36,16 +39,21 @@ fun View.getResourceDrawable(drawableID: Int) = this.context.getResourceDrawable
 
 fun View.getResourceString(stringID: Int) = this.context.getResourceString(stringID)
 
+fun View.getResourceStringArray(arrayId: Int): Array<String> = this.context.getResourceStringArray(arrayId)
+
 fun View.getResourceColorOrNull(colorId: Int) = this.context.getResourceColorOrNull(colorId)
 
 fun View.getResourceDrawableOrNull(drawableID: Int) = this.context.getResourceDrawableOrNull(drawableID)
 
 fun View.getResourceStringOrNull(stringID: Int) = this.context.getResourceStringOrNull(stringID)
 
+
 fun Fragment.getResourceColor(colorId: Int) = this.requireContext().getResourceColor(colorId)
 
 fun Fragment.getResourceDrawable(drawableID: Int) = this.requireContext().getResourceDrawable(drawableID)
 
 fun Fragment.getResourceString(stringID: Int) = this.requireContext().getResourceString(stringID)
+
+fun Fragment.getResourceStringArray(arrayId: Int): Array<String> = this.requireContext().getResourceStringArray(arrayId)
 
 val resourcesDisplayMetrics: DisplayMetrics get() = Resources.getSystem().displayMetrics
